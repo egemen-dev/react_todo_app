bad6c9037b74e0866445a10715dd4883e9bec35f
egemen-dev <egemenwasd@gmail.com>
2023-05-20 13:49:42 +0300

    Update test file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("Initial commit", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/Logic will go brrrr here/i);
   expect(linkElement).toBeInTheDocument();
 });
