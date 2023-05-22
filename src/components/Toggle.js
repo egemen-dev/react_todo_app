e2d2224dbf79c92f14c0122fa352f16a85cf418f
egemen-dev <egemenwasd@gmail.com>
2023-05-22 23:43:33 +0300

    Adjust toggle component

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -1,13 +1,6 @@
 export default function Toggle() {
-  // Toggle Dark Function
   function toggleDark() {
     document.querySelector("html").classList.toggle("dark");
-    console.log(document.querySelector("html").classList.contains("dark"));
-
-    localStorage.setItem(
-      "isDarkMode",
-      document.querySelector("html").classList.contains("dark")
-    );
   }
 
   return (
@@ -19,23 +12,41 @@ export default function Toggle() {
         onClick={toggleDark}
       />
 
-      <span class="absolute inset-y-0 start-0 z-10 m-1 inline-flex h-6 w-6 items-center justify-center rounded-full bg-white text-gray-400 transition-all peer-checked:start-6 peer-checked:text-green-600">
+      <span class="absolute inset-y-0 start-0 z-10 m-1 inline-flex h-6 w-6 items-center justify-center rounded-full bg-white text-gray-800 transition-all peer-checked:start-6 peer-checked:text-gray-800">
         <svg
           data-checked-icon
           xmlns="http://www.w3.org/2000/svg"
-          className="hidden h-4 w-4"
-          viewBox="0 0 20 20"
-          fill="currentColor"
+          fill="none"
+          viewBox="0 0 24 24"
+          strokeWidth={1.5}
+          stroke="currentColor"
+          className="hidden h-5 w-5"
         >
           <path
-            fill-rule="evenodd"
-            d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-            clip-rule="evenodd"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+          />
+        </svg>
+
+        <svg
+          data-unchecked-icon
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          className="h-5 w-5"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M21.752 15.002A9.718 9.718 0 0118 15.75c-5.385 0-9.75-4.365-9.75-9.75 0-1.33.266-2.597.748-3.752A9.753 9.753 0 003 11.25C3 16.635 7.365 21 12.75 21a9.753 9.753 0 009.002-5.998z"
           />
         </svg>
       </span>
 
-      <span className="absolute inset-0 rounded-full bg-gray-500 transition peer-checked:bg-green-500"></span>
+      <span className="absolute inset-0 rounded-full bg-slate-800 transition peer-checked:bg-gray-300"></span>
     </label>
   );
 }
