501460ae8395715f84609b16053916f515101373
egemen-dev <egemenwasd@gmail.com>
2023-05-22 21:59:59 +0300

    Implement to save rather than save button

--- src/App.js
+++ src/App.js
@@ -39,6 +39,7 @@ function App() {
 
     setTodoList([...todoList, newTodo]);
     todoInput.value = "";
+    localStorage.setItem("todoList", JSON.stringify([...todoList, newTodo]));
   }
 
   function handleDelete(e) {
@@ -46,6 +47,7 @@ function App() {
     const newList = todoList.filter((item) => item.id !== Number(todoId));
 
     setTodoList(newList);
+    localStorage.setItem("todoList", JSON.stringify(newList));
   }
 
   function handleEdit(e) {
@@ -56,6 +58,7 @@ function App() {
     todoInput.value = todoList.find((item) => item.id === Number(todoId)).content;
 
     setTodoList(newList);
+    localStorage.setItem("todoList", JSON.stringify(newList));
   }
 
   function handleCheckboxChange(e) {
@@ -68,15 +71,12 @@ function App() {
     });
 
     setTodoList(newList);
+    localStorage.setItem("todoList", JSON.stringify(newList));
   }
 
   function handleDeleteStorage() {
     localStorage.removeItem("todoList");
     setTodoList([]);
-  }
-
-  function handleSaveToStorage() {
-    localStorage.setItem("todoList", JSON.stringify(todoList));
   }
 
   return (
@@ -88,7 +88,7 @@ function App() {
         setList={setTodoList}
         animation="200"
         easing="ease-out"
-        className="w-full flex flex-col items-center gap-4 overflow-auto pt-6"
+        className="w-full flex flex-col items-center gap-6 overflow-auto pt-6"
       >
         {todoList.map((todo) => (
           <Task id={todo.id} key={todo.id}>
@@ -97,60 +97,52 @@ function App() {
               value={todo.isDone}
               onChange={handleCheckboxChange}
             />
-            <p className="dark:text-gray-100 col-span-6 md:col-span-7 break-words cursor-pointer">
+            <p className="dark:text-gray-100 col-span-6 break-words cursor-pointer">
               {todo.content}
             </p>
-            <div className="col-span-3 md:col-span-2 flex justify-end gap-4">
+            <div className="col-span-3 flex justify-end gap-4 min-w-max">
               <Button
-                color={"amber"}
+                color={"lime"}
                 dataKey={todo.id}
                 onClick={handleEdit}
-                icon={<Icon name="edit" size="sm" />}
+                icon={<Icon name="edit" size="md" dataKey={todo.id} />}
               />
               <Button
-                color={"light-red"}
+                color={"sunset"}
                 dataKey={todo.id}
                 onClick={handleDelete}
-                icon={<Icon name="delete" size="sm" />}
+                icon={<Icon name="delete" size="md" dataKey={todo.id} />}
               />
             </div>
           </Task>
         ))}
       </ReactSortable>
 
-      <form className="w-11/12 md:w-1/3 pt-6" onSubmit={handleSubmit}>
-        <Input
-          id="todoInput"
-          name="content"
-          button={
-            <Button
-              color={"blue"}
-              id="submitButton"
-              type="submit"
-              label={"Add"}
-              icon={<Icon name="add-plus" size="md" />}
-            />
-          }
+      <form
+        className="flex items-center justify-center w-11/12 md:w-1/3 pt-6"
+        onSubmit={handleSubmit}
+      >
+        <Input id="todoInput" name="content" />
+        <Button
+          color={"ocean"}
+          id="submitButton"
+          type="submit"
+          label={"Add"}
+          icon={<Icon name="add-plus" size="md" />}
         />
       </form>
 
       {todoList.length > 0 && (
         <div className="w-full col-span-3 flex justify-center gap-4 pt-6">
           <Button
-            color={"red"}
+            color={"sunset"}
             label="Empty List"
             icon={<Icon name="backspace" size="md" />}
             onClick={handleDeleteStorage}
           />
-          <Button
-            color={"green"}
-            label="Save List"
-            icon={<Icon name="bookmark" size="md" />}
-            onClick={handleSaveToStorage}
-          />
         </div>
       )}
-      {/* <Toggle></Toggle> */}
+      <Toggle></Toggle>
     </div>
   );
 }
