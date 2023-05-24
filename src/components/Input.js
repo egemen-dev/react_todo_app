0e093872b9c3ba3d45617e98d373005e3631e1c6
egemen-dev <egemenwasd@gmail.com>
2023-05-24 17:03:15 +0300

    Adjust padding

--- src/components/Input.js
+++ src/components/Input.js
@@ -1,10 +1,10 @@
 export default function Input({ id }) {
   return (
-    <div className="relative flex w-4/6 h-11">
+    <div className="relative flex w-3/4 h-12">
       <input
         type="input"
         placeholder="My new task"
-        className="w-full rounded-lg h-full px-2 mx-4 shadow-sm sm:text-sm focus:outline-none bg-gray-200 dark:bg-gray-700 dark:text-gray-100"
+        className="w-full rounded-xl h-full px-2 shadow-sm sm:text-sm focus:outline-none bg-gray-200 dark:bg-gray-700 dark:text-gray-100"
         autoComplete="off"
         id={id}
       />
--- src/components/Task.js
+++ src/components/Task.js
@@ -3,7 +3,7 @@ export default function Task({ id, key, children }) {
     <div
       id={id}
       key={key}
-      className="grid grid-cols-10 gap-2 items-center w-full xl:w-1/2 hover:bg-gray-200 dark:hover:bg-gray-700 rounded-lg px-4 py-2"
+      className="grid grid-cols-10 gap-2 items-center w-full md:w-1/2 hover:bg-gray-300 dark:hover:bg-gray-800 rounded-xl px-4 py-3"
     >
       {children}
     </div>
