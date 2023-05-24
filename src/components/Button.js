0572fb40f305cf641b268e05ddff10fcc6b269fa
egemen-dev <egemenwasd@gmail.com>
2023-05-24 17:02:03 +0300

    Change how button look

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,41 +1,16 @@
-export default function Button({ color, size, label, dataKey, onClick, icon }) {
-  function renderColor(color) {
-    switch (color) {
-      case "ocean":
-        return "relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white ";
-      case "lime":
-        return "relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-teal-300 to-lime-300 group-hover:from-teal-300 group-hover:to-lime-300 dark:text-white dark:hover:text-gray-900 ";
-      case "sunset":
-        return "relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-pink-500 to-orange-400 group-hover:from-pink-500 group-hover:to-orange-400 hover:text-white dark:text-white ";
-      case "yellow":
-        return "relative inline-flex items-center justify-center p-0.5 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-red-200 via-red-300 to-yellow-200 group-hover:from-red-200 group-hover:via-red-300 group-hover:to-yellow-200 dark:text-white dark:hover:text-gray-900 ";
-    }
-  }
-
-  function renderSize(size) {
-    switch (size) {
-      case "sm":
-        return "px-2 py-2";
-      case "md":
-        return "px-4 py-2";
-      case "lg":
-        return "px-4 py-2.5";
-    }
-  }
-
+export default function Button({ label, dataKey, onClick, icon }) {
   return (
-    <button className={renderColor(color)} data-key={dataKey} onClick={onClick}>
-      <span
-        data-key={dataKey}
-        className={`relative ${renderSize(
-          size
-        )} transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0`}
-      >
-        <div className="flex flex-row gap-1">
-          {label}
-          {icon}
-        </div>
-      </span>
+    <button
+      className={
+        "relative inline-flex items-center justify-center p-2.5 overflow-hidden text-sm font-medium text-gray-900 rounded-xl bg-gray-200 hover:bg-gray-100 dark:bg-gray-900 dark:text-gray-300 dark:hover:bg-gray-700 "
+      }
+      data-key={dataKey}
+      onClick={onClick}
+    >
+      <div className="flex flex-row gap-1">
+        {label}
+        {icon}
+      </div>
     </button>
   );
 }
